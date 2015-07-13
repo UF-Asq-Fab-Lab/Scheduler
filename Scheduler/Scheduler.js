ffb8478f200ce2fc29bf21ae50c5cc167e876d54
thomasrstorey <storey.thomas@gmail.com>
2015-07-12 21:18:29 -0400

    process pages dont install for some reason, still havent tested pushing new events, but the frontend looks correct at the very least.

--- Scheduler/Scheduler.js
+++ Scheduler/Scheduler.js
@@ -1,15 +1,17 @@
+var config = {scheduler_format: "M-D-YYYY h:mm a"};
+
 function overlay (start, end) {
   el = $("#overlay");
   el.css("visibility", function(){return (el.css("visibility") == "visible") ? "hidden" : "visible"});
   el = $("#overlay-bg");
   el.css("visibility", function(){return (el.css("visibility") == "visible") ? "hidden" : "visible"});
-  $('[name=start]').val(start.format(config.scheduler_format));
-  $('[name=end]').val(end.format(config.scheduler_format));
+  $('[name=start_time]').val(start.format(config.scheduler_format));
+  $('[name=end_time]').val(end.format(config.scheduler_format));
 };
 
 function reserve (event) {
-  $("#start").val(moment($("#start").val(), config.scheduler_format).unix());
-  $("#end").val(moment($("#end").val(), config.scheduler_format).unix());
+  $("#start_time").val(moment($("#start_time").val(), config.scheduler_format).unix());
+  $("#end_time").val(moment($("#end_time").val(), config.scheduler_format).unix());
   //event.preventDefault();
 }
 
@@ -25,25 +27,13 @@ function reloadCalendar () {
 };
 
 $(document).ready(function() {
-    events = events.map(function (cv, i) {
-      cv.start = moment.unix(parseInt(cv.start)).format();
-      cv.end = moment.unix(parseInt(cv.end)).format();
-      cv = {
-        title : cv.title,
-        start : cv.start,
-        end : cv.end
-      };
-      console.log(cv);
-      return cv;
-    });
+  $.getJSON("./", {config: 1}, function(data){
+    config = data;
+  });
      // page is now ready, initialize the calendar...
-    $("#overlaycancel").click(overlaycancel);
+    $("#overlay-cancel").click(overlaycancel);
 
     $("#form-reserve").submit(reserve);
-
-    equipment.forEach(function(eq){
-      $('#equipmentdropdown').append("<option>"+eq.equipment_name+"</option>");
-    });
 
     $('#calendar').fullCalendar({
         // put options and callbacks here
@@ -58,8 +48,20 @@ $(document).ready(function() {
           overlay(start, end.add(30, 'm'));
       },
       editable: false,
-      events: events, //events source
+      events: {
+        url: './',
+        type: 'GET',
+        data: {
+            events: 1,
+        },
+        error: function(d, e) {
+            alert('there was an error while fetching events!');
+            console.dir(e);
+        },
+      }, //events source
       timeFormat : config.scheduler_format
     });
-
+    $("#main-mesh").remove();
+    mainMesh = MeshGen("#main", 20);
+    mainMesh.meshify();
 });
